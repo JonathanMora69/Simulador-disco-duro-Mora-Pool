5925a29fd1dfcdb9c74f560953ab1feee8c28d42
jonat <jonat@DESKTOP-KQHBUI5>
2023-06-04 18:56:34 -0500

    Agregar archivos de proyecto.

---
+++ Simulador disco duro/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Simulador_disco_duro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelFileName = new System.Windows.Forms.Label();
+            labelFileSize = new System.Windows.Forms.Label();
+            textBoxFileSize = new System.Windows.Forms.TextBox();
+            textBoxFileName = new System.Windows.Forms.TextBox();
+            buttonSave = new System.Windows.Forms.Button();
+            listViewSectors = new System.Windows.Forms.ListView();
+            listViewFiles = new System.Windows.Forms.ListView();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // labelFileName
+            // 
+            labelFileName.AutoSize = true;
+            labelFileName.Location = new System.Drawing.Point(12, 23);
+            labelFileName.Name = "labelFileName";
+            labelFileName.Size = new System.Drawing.Size(112, 15);
+            labelFileName.TabIndex = 0;
+            labelFileName.Text = "Nombre del archivo";
+            // 
+            // labelFileSize
+            // 
+            labelFileSize.AutoSize = true;
+            labelFileSize.Location = new System.Drawing.Point(146, 23);
+            labelFileSize.Name = "labelFileSize";
+            labelFileSize.Size = new System.Drawing.Size(110, 15);
+            labelFileSize.TabIndex = 1;
+            labelFileSize.Text = "Tamaño del archivo";
+            // 
+            // textBoxFileSize
+            // 
+            textBoxFileSize.Location = new System.Drawing.Point(146, 41);
+            textBoxFileSize.Name = "textBoxFileSize";
+            textBoxFileSize.Size = new System.Drawing.Size(100, 23);
+            textBoxFileSize.TabIndex = 2;
+            // 
+            // textBoxFileName
+            // 
+            textBoxFileName.Location = new System.Drawing.Point(12, 41);
+            textBoxFileName.Name = "textBoxFileName";
+            textBoxFileName.Size = new System.Drawing.Size(100, 23);
+            textBoxFileName.TabIndex = 3;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Location = new System.Drawing.Point(280, 41);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new System.Drawing.Size(75, 23);
+            buttonSave.TabIndex = 4;
+            buttonSave.Text = "Guardar";
+            buttonSave.UseVisualStyleBackColor = true;
+            buttonSave.Click += buttonSave_Click;
+            // 
+            // listViewSectors
+            // 
+            listViewSectors.HideSelection = false;
+            listViewSectors.Location = new System.Drawing.Point(12, 115);
+            listViewSectors.Name = "listViewSectors";
+            listViewSectors.Size = new System.Drawing.Size(244, 167);
+            listViewSectors.TabIndex = 5;
+            listViewSectors.UseCompatibleStateImageBehavior = false;
+            // 
+            // listViewFiles
+            // 
+            listViewFiles.HideSelection = false;
+            listViewFiles.Location = new System.Drawing.Point(332, 115);
+            listViewFiles.Name = "listViewFiles";
+            listViewFiles.Size = new System.Drawing.Size(258, 167);
+            listViewFiles.TabIndex = 6;
+            listViewFiles.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(642, 48);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(113, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Mora Pool Jonathan";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(642, 75);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(126, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Adamaris Gomez Leon";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.gatito_fachero;
+            pictureBox1.Location = new System.Drawing.Point(628, 141);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(160, 118);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(listViewFiles);
+            Controls.Add(listViewSectors);
+            Controls.Add(buttonSave);
+            Controls.Add(textBoxFileName);
+            Controls.Add(textBoxFileSize);
+            Controls.Add(labelFileSize);
+            Controls.Add(labelFileName);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFileName;
+        private System.Windows.Forms.Label labelFileSize;
+        private System.Windows.Forms.TextBox textBoxFileSize;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.ListView listViewSectors;
+        private System.Windows.Forms.ListView listViewFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Simulador disco duro/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulador_disco_duro
+{
+    public partial class Form1 : Form
+    {
+        private const int SectorSize = 512;
+        private const int MaxSectors = 100;
+
+        private List<FileEntry> files;
+        private List<Sector> sectors;
+
+        public Form1()
+        {
+            InitializeComponent();
+            files = new List<FileEntry>();
+            sectors = new List<Sector>();
+            InitializeSectors();
+            UpdateSectorLabels();
+        }
+
+        private void InitializeSectors()
+        {
+            for (int i = 0; i <= MaxSectors; i++)
+            {
+                sectors.Add(new Sector());
+            }
+        }
+
+        private void UpdateSectorLabels()
+        {
+            listViewSectors.Items.Clear();
+
+            for (int i = 0; i < sectors.Count; i++)
+            {
+                string status = sectors[i].IsOccupied ? "Ocupado" : "Libre";
+                string[] row = { i.ToString(), status };
+                var listViewItem = new ListViewItem(row);
+                listViewSectors.Items.Add(listViewItem);
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            string fileName = textBoxFileName.Text.Trim();
+            int fileSize = Convert.ToInt32(textBoxFileSize.Text);
+
+            if (fileName.Length == 0 || fileSize <= 0)
+            {
+                MessageBox.Show("Ingresa un nombre de archivo válido y un tamaño mayor a cero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (sectors.Count(s => !s.IsOccupied) == 0)
+            {
+                MessageBox.Show("No hay suficiente espacio disponible en el disco.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (files.Any(f => f.FileName == fileName))
+            {
+                MessageBox.Show("El nombre de archivo ya existe.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int requiredSectors = (int)Math.Ceiling((double)fileSize / SectorSize);
+
+            var freeSectors = sectors.Where(s => !s.IsOccupied).ToList();
+
+            if (freeSectors.Count < requiredSectors)
+            {
+                MessageBox.Show("No hay suficiente espacio disponible en el disco para almacenar el archivo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var sectorsToOccupy = freeSectors.Take(requiredSectors).ToList();
+
+            FileEntry newFile = new FileEntry(fileName, sectorsToOccupy);
+            files.Add(newFile);
+
+            foreach (var sector in sectorsToOccupy)
+            {
+                sector.IsOccupied = true;
+                sector.AssociatedFile = newFile;
+            }
+
+            textBoxFileName.Clear();
+            textBoxFileSize.Clear();
+
+            UpdateSectorLabels();
+            UpdateFileList();
+        }
+
+        private void UpdateFileList()
+        {
+            listViewFiles.Items.Clear();
+
+            var sortedFiles = files.OrderBy(f => f.FileName).ToList();
+
+            foreach (var file in sortedFiles)
+            {
+                string[] row = { file.FileName, string.Join(", ", file.Sectors.Select(s => s.Index.ToString())) };
+                var listViewItem = new ListViewItem(row);
+                listViewFiles.Items.Add(listViewItem);
+            }
+        }
+    }
+
+    public class Sector
+    {
+        public int Index { get; set; }
+        public bool IsOccupied { get; set; }
+        public FileEntry AssociatedFile { get; set; }
+
+        public Sector()
+        {
+            IsOccupied = false;
+        }
+    }
+
+    public class FileEntry
+    {
+        public string FileName { get; set; }
+        public List<Sector> Sectors { get; set; }
+
+        public FileEntry(string fileName, List<Sector> sectors)
+        {
+            FileName = fileName;
+            Sectors = sectors;
+        }
+    }
+}
